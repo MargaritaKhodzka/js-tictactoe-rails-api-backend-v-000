276cab511335a9bb70cace131dc063c7511dad8c
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-24 15:22:58 -0400

    games controller index and create actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,23 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+    games = Game.all
+    render json:games
+  end
+
+  def new
+    game = Game.new
+  end
+
+  def create
+    game = Game.create(game_params)
+    render json: game
+  end
+
+  private
+  def game_params
+    params.permit(state: [])
+  end
+
 end
